636f3d09623307eae6e2ca88a31c7473e5ced7fe
Diego Kremer <diego.kremer@gmail.com>
2018-05-07 16:44:57 -0300

    Métodos Clutter e Split
    Criados métodos clutter para randomizar dados e split para dividir os dados nos sets de treino, teste e validação

--- machine_learning/model_generator.py
+++ machine_learning/model_generator.py
@@ -1,27 +1,45 @@
 import numpy as np
-import nltk as nltk
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import tensorflow as tf
 
 
 class ModelPreprocessing ():
+    
     
     def __init___ (self, dataset, label):
         self.dataset = dataset
         self.label = label
     
+    def clutter_data_order(self):
+        """
+            Randomize the order of the data in the dataset in case the data
+            comes sorted by any argument.
+        """
+        self.dataset = self.dataset.reindex(
+                np.random.permutation(self.dataset.index))
+        return self.dataset
     
-    def split_dataframe():
-        self.dataset
-        training_data = self.dataset.iloc[]
-        test_data = 
-        validation_data = 
+    def split_dataframe(self):
+        """
+            Split the dataset into three other datasets.
+                Training Dataset: 
+                    Data which will be used to train the model
+                Validation Dataset: 
+                    Data which will be used to validate results.
+                Test Dataset:
+                    Data that will be used to run a last test on 
+                    the model with the highest accuracy achieved. 
+        """
+        train_sample_ratio = 70
+        train_sample_volume = (self.dataset.count() * train_sample_ratio) / 100
+        training_data = self.dataset.head(train_sample_volume)
+        full_test_data = self.dataset.tail(self.dataset.count() - train_sample_volume)
+        test_sample_volume = (full_test_data.count() * 70) / 100
+        test_data = full_test_data.head(test_sample_volume)
+        validation_data = full_test_data.tail(full_test_data.count() - test_sample_volume)
         return training_data, test_data, validation_data
     
     
-    def process_label():
+    def process_label(self):
         label_frame = pd.DataFrame()
         label_frame.assign(self.dataset.at[self.label])
         return label_frame
